d1a5d5fd61de15d41fe5d52b2e0d8ce5d7a05920
Lohitha-kakumani <141537855+Lohitha-kakumani@users.noreply.github.com>
2025-06-24 18:56:44 +0530

    Time: 4 ms (11.84%), Space: 13 MB (64.69%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int m = dungeon.size();
-        int n = dungeon[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
-        
-        dp[m][n - 1] = dp[m - 1][n] = 1; 
-        
-        for (int i = m - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                int need = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
-                dp[i][j] = max(1, need);
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1)
+                    dp[i][j] = dungeon[i][j];                
+                else if(i==n-1)
+                    dp[i][j] = dungeon[i][j] + dp[i][j+1];                  
+                else if( j==m-1)
+                    dp[i][j] = dungeon[i][j] + dp[i+1][j];  
+                else
+                    dp[i][j] = dungeon[i][j] + max(dp[i+1][j], dp[i][j+1]);   
+                if(dp[i][j]>0)
+                    dp[i][j] = 0;
             }
         }
-        
-        return dp[0][0];
+        if(dp[0][0]>=0)
+            return 1;
+        else 
+            return (abs(dp[0][0])+1);
     }
 };
